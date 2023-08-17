c132e19728a2e0ec5004634fbac2826eff80c903
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-08-17 15:18:47 +0530

    using MySQL for the first time in this backend

---
+++ products.js
@@ -0,0 +1,24 @@
+const products = [
+    ['Trimmer', 'trimmer.jpg', 39.99, 50],
+    ['Charger', 'charger.jpg', 9.99, 100],
+    ['Remote Control', 'remote.jpg', 14.99, 75],
+    ['Headphones', 'headphones.jpg', 29.99, 200],
+    ['Smartphone', 'smartphone.jpg', 299.99, 25],
+    ['Laptop', 'laptop.jpg', 899.99, 20],
+    ['Mouse', 'mouse.jpg', 9.99, 150],
+    ['Keyboard', 'keyboard.jpg', 19.99, 100],
+    ['Speaker', 'speaker.jpg', 49.99, 50],
+    ['Camera', 'camera.jpg', 199.99, 30],
+    ['Tablet', 'tablet.jpg', 149.99, 40],
+    ['Watch', 'watch.jpg', 79.99, 60],
+    ['TV', 'tv.jpg', 499.99, 15],
+    ['Printer', 'printer.jpg', 79.99, 40],
+    ['Vacuum Cleaner', 'vacuum.jpg', 89.99, 35],
+    ['Microwave', 'microwave.jpg', 129.99, 25],
+    ['Toaster', 'toaster.jpg', 24.99, 70],
+    ['Blender', 'blender.jpg', 34.99, 55],
+    ['Fan', 'fan.jpg', 19.99, 90],
+    ['Iron', 'iron.jpg', 14.99, 85]
+];
+
+module.exports =  products;
