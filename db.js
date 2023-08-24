e5d8b325eb8ea63ddaf8d75f303e5e3268fa03f5
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-08-24 22:17:27 +0530

    changed database hosting

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const mysql = require('mysql');
 
+// Replace these values with the credentials from your PlanetScale project
+const host = "aws.connect.psdb.cloud";
+const username = "d38fs9ahmql5979fxo2p";
+const password = "pscale_pw_l7lhyfL2TGenrE8dQ64QQoiQ2cJvh9PwObzyH3YCOt3";
+const database = "products-management";
+
 const db = mysql.createConnection({
-    host: "sql6.freesqldatabase.com",
-    user: "sql6640348",
-    password: "YwTRkibeJI",
-    database: "sql6640348",
+    host: host,
+    user: username,
+    password: password,
+    database: database,
+    // Add this option to the connection configuration to enable SSL
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = db;
